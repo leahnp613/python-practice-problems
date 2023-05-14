84c38c8f2e3780f45ee176a53d97be13dedeaaf9
leahnp613 <leahnagarpowers@gmail.com>
2023-05-14 14:21:19 -0400

    added solution from folder for problem 20, defined variables before finding 50% of list

--- problems/problem_020.py
+++ problems/problem_020.py
@@ -4,4 +4,25 @@
 
 
 def has_quorum(attendees_list, members_list):
+  #define has_quorum
+  #len of each list
+  #if len of attendees list is 50% of len of members list
+  #if num in attendees_list >= .5 
     pass
+
+
+def has_quorum(attendees_list, members_list):
+    # num_attendees = the number of attendees
+    num_attendees = len(attendees_list)  # solution
+    # num_members = the number of members
+    num_members = len(members_list)  # solution
+    # If the num_attendees divided by the num_members is
+    # greater than 0.5
+    if num_attendees / num_members >= 0.5:  # solution
+        # return True
+        return True  # solution
+    # Otherwise
+    else:  # solution
+        # return False
+        return False  # solution
+    # pass                                              # problem
