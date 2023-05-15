67412d8a5f5d91e1c9f8c27ed22114a7931466a0
leahnp613 <leahnagarpowers@gmail.com>
2023-05-15 15:24:33 -0400

    solved problem 73

--- problems/problem_073.py
+++ problems/problem_073.py
@@ -21,3 +21,18 @@
 #
 # Do it without pseudocode, this time, from memory. Don't look
 # at the last one you just wrote unless you really must.
+
+from ast import Num
+
+
+class Student:
+    def _init_ (self, name):
+        self.name = name
+
+    def add_score(self, score):
+        self.score.append 
+    
+    def get_average(self, score):
+        if len(self.score) == 0:
+            return None
+        return sum(self.scores) / len(self.scores)
