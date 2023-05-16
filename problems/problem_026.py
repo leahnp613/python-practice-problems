1079c1cb52dd1d2ab1e14abbc8f274348925a1bc
leahnp613 <leahnagarpowers@gmail.com>
2023-05-16 11:40:59 -0400

    solution for problem 27, added solution to study

--- problems/problem_026.py
+++ problems/problem_026.py
@@ -14,4 +14,20 @@
 
 
 def calculate_grade(values):
-    pass
+    if len(values) == 0:  # solution
+        return None  # solution
+    sum = 0  # solution
+    for item in values:  # solution
+        sum = sum + item  # solution
+    average = sum / len(values)  # solution
+    if average >= 90:  # solution
+        return "A"  # solution
+    elif average >= 80:  # solution
+        return "B"  # solution
+    elif average >= 70:  # solution
+        return "C"  # solution
+    elif average >= 60:  # solution
+        return "D"  # solution
+    else:  # solution
+        return "F"  # solution
+    # pass                                              # problem
--- problems/problem_027.py
+++ problems/problem_027.py
@@ -5,5 +5,28 @@
 #
 
 
+#def max_in_list(values):
+ #   if len(values) == 0:
+  #      return None                            INITIAL ATTEMPT, SOLUTION BELOW TO STUDY
+   # max(values) == max_in_list()
+    
+#print(max_in_list([12, 13, 89]))
+#print(max_in_list([]))
+
+
 def max_in_list(values):
-    pass
+    # if there are no items in the values list
+    if len(values) == 0:  # solution
+        # return None
+        return None  # solution
+    # max value = first item in the values list
+    max_value = values[0]  # solution
+    # for each item in the values list
+    for item in values:  # solution
+        # if item is greater than the max value
+        if item > max_value:  # solution
+            # max value = item
+            max_value = item  # solution
+    # return the max value
+    return max_value  # solution
+    # pass                                              # problem
