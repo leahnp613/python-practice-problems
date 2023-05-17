147cbe91c179a440e1a4d2669c9265b4f0d5d7ba
leahnp613 <leahnagarpowers@gmail.com>
2023-05-17 12:52:22 -0400

    remove duplicates

--- problems/problem_061.py
+++ problems/problem_061.py
@@ -14,4 +14,10 @@
 #     * input:   [1, 3, 3, 20, 3, 2, 2]
 #       returns: [1, 3, 20, 2]
 
-def remove_duplicates(values):
+
+def remove_duplicates(values):  # solution
+    output = []  # solution
+    for value in values:  # solution
+        if value not in output:  # solution
+            output.append(value)  # solution
+    return output  # solution
