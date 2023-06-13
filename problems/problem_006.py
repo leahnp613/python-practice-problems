eb54f61a7584e746b84862992b9e559259bb40bc
leahnp613 <leahnagarpowers@gmail.com>
2023-06-13 10:24:13 -0400

    solved problem 6

--- problems/problem_006.py
+++ problems/problem_006.py
@@ -10,4 +10,9 @@
 # problem to get a good feel for how to solve it.
 
 def can_skydive(age, has_consent_form):
-    pass
+    if age >= 18 and has_consent_form == True:
+        return True
+
+print(can_skydive(21, True))
+print(can_skydive(16,True))
+print(can_skydive(45, False))
