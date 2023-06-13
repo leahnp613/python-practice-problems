e232469568eaa59a9a48498d1419a65aea95444b
leahnp613 <leahnagarpowers@gmail.com>
2023-06-13 11:30:05 -0400

    solved problem 12

--- problems/problem_012.py
+++ problems/problem_012.py
@@ -16,4 +16,13 @@
 # problem to get a good feel for how to solve it.
 
 def fizzbuzz(number):
-    pass
+    if number % 3 == 0 and number % 5 == 0:
+        return "fizzbuzz"
+    elif number % 5 == 0:
+        return "buzz"
+    elif number % 3 == 0:
+        return "fizz"
+ 
+    else: 
+        return number
+   
